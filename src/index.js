5136ab8a46b351a5bfac6a71353e3db132de023f
何雷 <595806119@qq.com>
2018-09-08 17:36:58 +0800

    组建测试

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+const systemInfo = wx.getSystemInfoSync()
+const SCREEN_WIDTH = systemInfo.windowWidth
+// const SCREEN_HEIGHT = systemInfo.windowHeight
+
 Component({
   options: {
     multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  externalClasses: ['wrap-container'],
+  data: {
+    itemWidth: SCREEN_WIDTH
   },
   properties: {
     prop: {
       type: String,
       value: 'index.properties'
     },
+    dataList: {
+      type: Array,
+      value: []
+    },
+    reduceDistance: {
+      type: Number,
+      value: 0,
+      observer: (newVal, oldVal) => {
+        this.setData({
+          itemWidth
+        })
+      }
+    }
   },
   methods: {
 
@@ -20,5 +41,8 @@ Component({
         }
       })
     }
+  },
+  pageLifetimes: {
+
   }
 })
